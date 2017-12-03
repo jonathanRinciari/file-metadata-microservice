9c2d60784fc7a15976723dae0fad39e50fff4219
jon <jrinciari@or-specialties.com>
2017-12-03 21:11:04 +0000

    added a pug file to contain the upload form

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -1,3 +1,11 @@
 exports.index = function(req, res){
     res.send('api route from controller')
 }
+
+exports.upload = function(req, res){
+    res.render('upload')
+}
+
+exports.results = function(req, res){
+    res.send('results route')
+}
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ var api_controller = require('../controllers/api_controller')
 
 router.get('/', api_controller.index)
 
+router.get('/upload', api_controller.upload)
 
+router.get('/results', api_controller.results)
 
 module.exports = router;
